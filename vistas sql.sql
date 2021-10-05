0dff878393f3f4117b3b2211a46d20745beb5b54
OscarDan3 <ob667037@gmail.com>
2021-10-05 14:35:13 -0600

    cree vistas sobre las tablas de mi base de datos

--- vistas sql.sql
+++ vistas sql.sql
@@ -55,3 +55,39 @@ ON p.CategoryID = c.CategoryID;
 SELECT * FROM listadoProducto
 
 drop view listadoProducto
+
+--Usando mi base de datos
+use Biblioteca3
+
+--Mostrar nombre y appellido de la tabla encargado y municipio y departamento de la tabla direccion_encargado
+SELECT
+	  registroBiblioteca.encargado.nombres Nombres,
+	   registroBiblioteca.encargado.apellidos,
+      registroBiblioteca.direccion_encargado.municipio Municipio,
+	  registroBiblioteca.direccion_encargado.departamento Departamento
+
+FROM
+	registroBiblioteca.encargado
+INNER JOIN registroBiblioteca.direccion_encargado
+ON registroBiblioteca.direccion_encargado.cod_direccion_encargado=registroBiblioteca.encargado.idencargado;
+
+---Creando vista usando las tablas encargado,direccion_encardo y encargado1,para mostrar nombre y appellido de la tabala encargado,y mostrar de la tabala direccion_encardo el departamento y mostrar la fecha_nacimiento de la tabala encargado1
+CREATE VIEW datosencargado
+AS
+SELECT
+       registroBiblioteca.encargado.nombres Nombres,
+	   registroBiblioteca.encargado.apellidos,
+	  registroBiblioteca.direccion_encargado.departamento Departamento,
+	  registroBiblioteca.encargado1.fecha_nacimiento
+
+  
+FROM
+    registroBiblioteca.encargado
+	
+INNER JOIN registroBiblioteca.direccion_encargado
+ON  cod_direccion_encargado= cod_direccion_encargado
+INNER JOIN registroBiblioteca.encargado1
+ON  registroBiblioteca.encargado1.idencargado= registroBiblioteca.encargado1.idencargado
+GROUP BY  registroBiblioteca.encargado.nombres, registroBiblioteca.encargado.apellidos,  registroBiblioteca.direccion_encargado.departamento,  registroBiblioteca.encargado1.fecha_nacimiento
+
+SELECT * FROM datosencargado
